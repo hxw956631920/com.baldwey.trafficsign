f0aa718ddce6a2f35bd97daaa4310cbbd7a1b795
黄新伟 <956631920@qq.com>
2020-03-21 18:36:45 +0800

    1

--- gtsrb.py
+++ gtsrb.py
@@ -90,13 +90,14 @@ def camera():
 
 def run(picPath, net, width = None, height = None):
     # 读取图片
-    image = Image.open(data_root+picPath)
-    image.resize((48,48), Image.NEAREST)
+    # image = Image.open(data_root+picPath)
+    # image.resize((48,48), Image.NEAREST)
     # caffe.io加载的图片为0～1之间 RGB格式 cv2.imread加载的图片为0～255之间 通道格式为(H,W,C)，即行，列、通道数(Row, Col, C)。
     # 这段为cv2.imread读取的图片与caffe读取的图片的格式转换
-    # image1=cv2.imread(caffe_root + 'examples/images/cat.jpg')  
-    # image1=cv2.cvtColor(image1,cv2.COLOR_BGR2RGB)  
-    # image1=image1/255
+    image=cv2.imread(data_root+picPath)  
+    image=cv2.resize(image, (48, 48))
+    image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  
+    image=image/255
     # image = caffe.io.load_image(data_root+picPath)
     # 设置输入图片大小
     ifnil(width, 48)
