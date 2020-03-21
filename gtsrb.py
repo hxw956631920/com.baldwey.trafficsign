06030b369cf3ed71b57a3d623d80ed0917d1d14d
黄新伟 <956631920@qq.com>
2020-03-21 16:52:12 +0800

    10

--- gtsrb.py
+++ gtsrb.py
@@ -22,6 +22,15 @@ caffe_root = '/home/baldwey/caffe_git/caffe/'
 
 sys.path.insert(0, caffe_root + '/python')
 
+# 测试模型
+model_def = data_root + 'model/deploy.prototxt'
+# 权重
+model_weights = data_root + '_iter_9000.caffemodel'
+# mean.binaryproto的路径
+model_mean_input = data_root + 'data/mean.binaryproto'
+# 转换为.npy的输出路径
+model_mean_output = data_root + 'data/mean.npy'
+
 net = None
 
 def ifnil(t, default):
@@ -52,6 +61,17 @@ def transform(path, net):
     transformer.set_channel_swap('data', (2, 1, 0))
     return transformer
 
+def initCaffeNet():
+    # 设置cpu模式
+    caffe.set_mode_cpu()
+    # 转换binaryproto 文件为 npy文件 如果转换过则跳过这一步
+    if os.path.exists(model_mean_output):
+       npyfile = binary2npy(model_mean_input, model_mean_output)
+       print("#########binaryproto file to npyfile finish#######")
+    # 初始化网络
+    net = caffe.Net(model_def, model_weights, caffe.TEST)
+    return net
+
 def camera():
     # 初始化摄像头
     cap = cv2.VideoCapture(0)
@@ -68,23 +88,7 @@ def camera():
             print("take picture finish")
             break
 
-def run(picPath, width = None, height = None):
-    # 设置cpu模式
-    caffe.set_mode_cpu()
-    # 测试模型
-    model_def = data_root + 'model/deploy.prototxt'
-    # 权重
-    model_weights = data_root + '_iter_9000.caffemodel'
-    # mean.binaryproto的路径
-    model_mean_input = data_root + 'data/mean.binaryproto'
-    # 转换为.npy的输出路径
-    model_mean_output = data_root + 'data/mean.npy'
-    # 转换binaryproto 文件为 npy文件 如果转换过则跳过这一步
-    if os.path.exists(model_mean_output):
-       npyfile = binary2npy(model_mean_input, model_mean_output)
-       print("#########binaryproto file to npyfile finish#######")
-    # 初始化网络
-    net = caffe.Net(model_def, model_weights, caffe.TEST)
+def run(picPath, net, width = None, height = None):
     # 读取图片
     im = Image.open(data_root+picPath)
     # caffe.io加载的图片为0～1之间 RGB格式 cv2.imread加载的图片为0～255之间 通道格式为(H,W,C)，即行，列、通道数(Row, Col, C)。
@@ -102,32 +106,35 @@ def run(picPath, width = None, height = None):
     # 设置数据读取层的形状
     net.blobs['data'].reshape(1,3,width,height) 
     # 均值处理过的结果
-    # transformer = transform(model_mean_output, net)
+    transformer = transform(model_mean_output, net)
+    # 对图片进行去均值处理
+    transformed_image = transformer.preprocess('data', image)
+    # 拷贝图像数据到网络层
+    net.blobs['data'].data[...] = transformed_image
+    # 前向传播计算得到结果
+    output = net.forward()
+    # 从softmax层在该模型中命名为prob 将结果取出
+    output_prob = output['prob'][0]
+    # 打印结果
+    print 'predicted class is:', output_prob.argmax()
+    # 对比标签文件中其它5个高可能性的结果
+    labels_file = data_root + 'data/val.txt'
+    if os.path.exists(labels_file):
+        labels = np.loadtxt(labels_file, str, delimiter='\r')
+        print 'output lable:', labels[output_prob.argmax()]
+    top_inds = output_prob.argsort()[::-1][:5]
+    print 'probabilities and labels', zip(output_prob[top_inds], labels[top_inds])
     # 显示图片
     plt.imshow(image)
     plt.show()
-    # # 对图片进行去均值处理
-    # transformed_image = transformer.preprocess('data', image)
-    # # 拷贝图像数据到网络层
-    # net.blobs['data'].data[...] = transformed_image
-    # # 前向传播计算得到结果
-    # output = net.forward()
-    # # 从softmax层在该模型中命名为prob 将结果取出
-    # output_prob = output['prob'][0]
-    # # 打印结果
-    # print 'predicted class is:', output_prob.argmax()
-    # # 对比标签文件中其它5个高可能性的结果
-    # labels_file = data_root + 'data/val.txt'
-    # if os.path.exists(labels_file):
-    #     labels = np.loadtxt(labels_file, str, delimiter='\r')
-    #     print 'output lable:', labels[output_prob.argmax()]
-    # top_inds = output_prob.argsort()[::-1][:5]
-    # print 'probabilities and labels', zip(output_prob[top_inds], labels[top_inds])
 
 # 参数1 flag 模式： 模式1 图片模式 模式2 摄像头模式
 # 参数2 picPath 图片地址 (图片模式为图片所在地址 摄像头模式为摄像头保存的图片地址)
 def main():  
-    run("Final_Test/Images/00002.ppm")
+    # 初始化caffe net
+    net = initCaffeNet()
+    # 运行计算
+    run("Final_Test/Images/00002.ppm", net)
     # if len(sys.argv)<=3:
     #     if len():
     #         pass
