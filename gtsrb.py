f3c3fda6443850e03887749eafcb59e315afaeab
黄新伟 <956631920@qq.com>
2020-03-23 19:30:42 +0800

    1

--- gtsrb.py
+++ gtsrb.py
@@ -134,7 +134,7 @@ def main():
     net = initCaffeNet()
     # 运行计算
     # run("camera_img/0.png", net, 48, 48)
-    run("/Final_Test/Images/00002", net, 48, 48)
+    run("/Final_Test/Images/00002.ppm", net, 48, 48)
     # if len(sys.argv)<=3:
     #     if len():
     #         pass
