49f496a478f64f4ceac957298e0cba7add2fe131
黄新伟 <956631920@qq.com>
2020-03-23 19:12:45 +0800

    1

--- gtsrb.py
+++ gtsrb.py
@@ -95,34 +95,34 @@ def run(picPath, net, width = None, height = None):
     # caffe.io加载的图片为0～1之间 RGB格式 cv2.imread加载的图片为0～255之间 通道格式为(H,W,C)，即行，列、通道数(Row, Col, C)。
     # 这段为cv2.imread读取的图片与caffe读取的图片的格式转换
     image=cv2.imread(data_root+picPath)  
-    image=cv2.resize(image, (144, 144))
+    image=cv2.resize(image, (48, 48), 0, 0, interpolation=cv2.INTER_AREA)
     # image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  
-    image=image/255
-    # image = caffe.io.load_image(data_root+picPath)
-    # 设置输入图片大小
-    ifnil(width, 144)
-    ifnil(height, 144)
-    # 设置数据读取层的形状
-    net.blobs['data'].reshape(1,3,width,height) 
-    # 均值处理过的结果
-    transformer = transform(model_mean_output, net)
-    # 对图片进行去均值处理
-    transformed_image = transformer.preprocess('data', image)
-    # 拷贝图像数据到网络层
-    net.blobs['data'].data[...] = transformed_image
-    # 前向传播计算得到结果
-    output = net.forward()
-    # 从softmax层在该模型中命名为prob 将结果取出
-    output_prob = output['prob'][0]
-    # 打印结果
-    print 'predicted class is:', output_prob.argmax()
-    # 对比标签文件中其它5个高可能性的结果
-    labels_file = data_root + 'data/val.txt'
-    if os.path.exists(labels_file):
-        labels = np.loadtxt(labels_file, str, delimiter='\r')
-        print 'output lable:', labels[output_prob.argmax()]
-    top_inds = output_prob.argsort()[::-1][:5]
-    print 'probabilities and labels', zip(output_prob[top_inds], labels[top_inds])
+    # image=image/255
+    # # image = caffe.io.load_image(data_root+picPath)
+    # # 设置输入图片大小
+    # ifnil(width, 144)
+    # ifnil(height, 144)
+    # # 设置数据读取层的形状
+    # net.blobs['data'].reshape(1,3,width,height) 
+    # # 均值处理过的结果
+    # transformer = transform(model_mean_output, net)
+    # # 对图片进行去均值处理
+    # transformed_image = transformer.preprocess('data', image)
+    # # 拷贝图像数据到网络层
+    # net.blobs['data'].data[...] = transformed_image
+    # # 前向传播计算得到结果
+    # output = net.forward()
+    # # 从softmax层在该模型中命名为prob 将结果取出
+    # output_prob = output['prob'][0]
+    # # 打印结果
+    # print 'predicted class is:', output_prob.argmax()
+    # # 对比标签文件中其它5个高可能性的结果
+    # labels_file = data_root + 'data/val.txt'
+    # if os.path.exists(labels_file):
+    #     labels = np.loadtxt(labels_file, str, delimiter='\r')
+    #     print 'output lable:', labels[output_prob.argmax()]
+    # top_inds = output_prob.argsort()[::-1][:5]
+    # print 'probabilities and labels', zip(output_prob[top_inds], labels[top_inds])
     # 显示图片
     plt.imshow(image)
     plt.show()
@@ -131,7 +131,7 @@ def run(picPath, net, width = None, height = None):
 # 参数2 picPath 图片地址 (图片模式为图片所在地址 摄像头模式为摄像头保存的图片地址)
 def main():  
     # 初始化caffe net
-    net = initCaffeNet()
+    # net = initCaffeNet()
     # 运行计算
     run("camera_img/0.png", net, 144, 144)
     # if len(sys.argv)<=3:
